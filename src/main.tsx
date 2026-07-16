c6c2957978ca6ce7894cf4c12a2fb46d3e4bd3fc
santhosh3 <santhosh.chinna@thoughtworks.com>
2026-07-16 15:53:07 +0530

    chore: made changes

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+import './index.css'
+import App from './App.tsx'
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:4000/" }),
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </StrictMode>,
+)
